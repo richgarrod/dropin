f01d548800aaacda22c3ba66b377b29d6a772982
richgarrod <=>
2016-07-03 13:41:06 +0100

    Changing port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 var home = require("./server/api/home");
 var about = require("./server/api/about");
 var boxes = require("./server/api/boxes");
@@ -15,6 +17,6 @@ app.get('*', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Dropin listening on port 3000!');
 });
